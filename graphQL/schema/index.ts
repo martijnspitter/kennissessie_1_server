3df1c1764626de5e95614ace333038113a51e3ed
Martijn Spitter <martijn@wearetheguild.net>
2021-10-07 16:36:26 +0200

    only account related schema

--- graphQL/schema/index.ts
+++ graphQL/schema/index.ts
@@ -10,41 +10,9 @@ export const typeDefs = gql`
     updatedAt: Int!
   }
 
-  type Profile {
-    id: ID!
-    firstname: String!
-    lastname: String!
-    account: Account
-    certificateRecipients: [Account]
-    createdAt: Int!
-    updatedAt: Int!
-  }
-
   input AccountInput {
     email: String!
     identifier: String
-  }
-
-  input ProfileInput {
-    firstname: String!
-    lastname: String!
-    account: ID!
-    certificateRecipients: [ID]
-  }
-
-  # input of validate endpoint
-  input ValidateCertificatie {
-    certificate: String!
-    publisherIdentity: String!
-    publisherAccountId: ID!
-  }
-
-  # input of publish certificate endpoint
-  input PublishCertificate {
-    publisherIdentity: String!
-    receiverIdentity: String!
-    receiverAccountId: ID!
-    publisherAccountId: ID!
   }
 
   input EditAccount {
@@ -52,31 +20,14 @@ export const typeDefs = gql`
     email: String
   }
 
-  input EditProfile {
-    profileId: ID!
-    firstname: String
-    lastname: String
-  }
-
   type Query {
     allAccounts: [Account!]!
     account(id: String!): Account
-    allProfiles: [Profile!]!
-    profile(id: String!): Profile
-    certificateRecipients(id: String!): [Account]!
-
-    # here is the validate endpoint with input and return: Boolean? 
-    validate(input: ValidateCertificatie): Boolean!
   }
 
   type Mutation {
     createAccount(input: AccountInput): Account!
     editAccount(input: EditAccount): Account!
-    createProfile(input: ProfileInput): Profile!
-    editProfile(input: EditProfile): Profile!
-    
-    # publish certificate endpoint. Return type now string
-    publishCertificate(input: PublishCertificate): String!
   }
 
 `;
