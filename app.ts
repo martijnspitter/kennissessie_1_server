2480d2e4a30fdd97ce4c74f2ce250fc32849b5ba
Martijn Spitter <martijn@wearetheguild.net>
2021-10-07 17:05:20 +0200

    updated app

--- app.ts
+++ app.ts
@@ -7,11 +7,6 @@ import { ApolloServer, AuthenticationError } from 'apollo-server-express';
 require('dotenv').config();
 
 const MONGO_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/kennissessie';
-console.log(MONGO_URL)
-var corsOptions = {
-  origin: '*',
-  credentials: true
-};
 
 async function startServer() {
   const app = express();
@@ -24,7 +19,7 @@ async function startServer() {
       const user = true
       if (!user) throw new AuthenticationError('you must be logged in');
 
-      return { user };
+      return { user, welkom: 'Welcome at TheGuild!' };
     }
   });
 
@@ -43,15 +38,3 @@ async function startServer() {
 
 startServer();
 
-// mongoose
-//   .connect(MONGO_URL)
-//   .then(() => {
-//     const PORT = process.env.PORT || 4000;
-//     server.listen(PORT, () => {
-//       console.log(`Server is running on port ${PORT}.`);
-//     });
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//     throw err;
-//   });
