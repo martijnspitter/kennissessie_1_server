c30fcae803fd56983109c002b8386d808391447d
Martijn Spitter <martijn@wearetheguild.net>
2021-10-07 16:35:17 +0200

    only account related resolvers

--- graphQL/resolvers/index.ts
+++ graphQL/resolvers/index.ts
@@ -35,60 +35,6 @@ const resolvers = {
         throw err;
       }
     },
-    allProfiles: async (parent: any, args: any, context: any, info: any) => {
-      try {
-        const profiles = await Profile.find().lean();
-        return profiles.map(async (profile: IProfile) => {
-          return {
-            ...profile,
-            account: getAccount(profile.account as Schema.Types.ObjectId),
-            certificateRecipients: getAccounts(profile.certificateRecipients as Schema.Types.ObjectId[])
-          }
-        })
-      } catch (err) {
-        console.log(err);
-        throw err;
-      }
-    },
-    profile: async (parent: any, args: any, context: any, info: any) => {
-      try {
-        const { id } = args;
-        const profile = await Profile.findById(id).lean();
-        if (profile)
-          return {
-            ...profile,
-            account: getAccount(profile.account as Schema.Types.ObjectId),
-          }
-        else return {}
-      } catch (err) {
-        console.log(err);
-        throw err;
-      }
-    },
-    certificateRecipients: async (parent: any, args: any, context: any, info: any) => {
-      try {
-        const { id } = args;
-        const profile = await Profile.findById(id);
-        if (profile) {
-          const recipients = await Profile.find({ _id: { $in: profile.certificateRecipients } }).lean()
-          return recipients.map(async (recipient: IProfile) => {
-            return {
-              ...profile,
-              account: getAccount(profile.account as Schema.Types.ObjectId),
-            }
-          })
-        } else return [];
-
-      } catch (err) {
-        console.log(err);
-        throw err;
-      }
-    },
-    validate: async (parent: any, args: any, context: any, info: any) => {
-      const { certificate, publisherIdentity, publisherAccountId } = args.input;
-      // validate certificate here
-      return true;
-    }
   },
   Mutation: {
     createAccount: async (parent: any, args: any, context: any, info: any) => {
@@ -98,24 +44,6 @@ const resolvers = {
         const { email } = args.input;
         const account = new Account({ email });
         return await account.save();
-      } catch (err) {
-        console.log(err);
-        throw err;
-      }
-    },
-    createProfile: async (parent: any, args: any, context: any, info: any) => {
-      try {
-        const { account, firstname, lastname } = args.input
-        const profile = new Profile({ account, firstname, lastname })
-        const createdProfile = await profile.save();
-        await Account.findByIdAndUpdate(account, { profile: createdProfile.id });
-        return {
-          ...createdProfile,
-          id: createdProfile._id,
-          firstname: createdProfile.firstname,
-          lastname: createdProfile.lastname,
-          account: getAccount(profile.account as Schema.Types.ObjectId),
-        }
       } catch (err) {
         console.log(err);
         throw err;
@@ -136,41 +64,7 @@ const resolvers = {
         throw err;
       }
     },
-    editProfile: async (parent: any, args: any, context: any, info: any) => {
-      try {
-        const { profileId, firstname, lastname } = args.input;
-        const profile = await Profile.findByIdAndUpdate(profileId, { firstname, lastname });
-        if (profile)
-          return {
-            ...profile,
-            account: getAccount(profile.account as Schema.Types.ObjectId),
-          }
-      } catch (err) {
-        console.log(err);
-        throw err;
-      }
-    },
-    publishCertificate: async (parent: any, args: any, context: any, info: any) => {
-      const { publisherIdentity, receiverIdentity, publisherAccountId, receiverAccountId } = args.input;
-      // create certificate
-
-
-      // add recipient to profile of publisher
-      const publisherAccount = await Account.findById(publisherAccountId).lean();
-      if (publisherAccount) {
-        const publisherProfile = await Profile.findById(publisherAccount.profile).lean();
-        if (publisherProfile) {
-          const recipients = [...publisherProfile.certificateRecipients];
-          recipients.push(receiverAccountId);
-          await Profile.updateOne({ id: publisherProfile.id }, { certificateRecipients: recipients })
-        }
-      }
-
-      // return certificate
-      return 'success';
-    }
   },
-
 }
 
 module.exports = resolvers;
